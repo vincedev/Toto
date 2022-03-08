7d7f0f604395e1c0d793943acae3625c14962a18
Vincent Carlier <vincent.carlier@kape.com>
2022-03-08 17:38:39 +0100

    Add XCFramework

---
+++ artifacts/Toto.xcframework/macos-arm64_x86_64/Toto.framework/Versions/A/Headers/Toto.h
@@ -0,0 +1,13 @@
+// 
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Toto.
+FOUNDATION_EXPORT double TotoVersionNumber;
+
+//! Project version string for Toto.
+FOUNDATION_EXPORT const unsigned char TotoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Toto/PublicHeader.h>
+
+
